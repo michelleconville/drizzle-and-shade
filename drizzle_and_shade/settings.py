c38bb36542db82c6faf5327093e86c2dfddb8eeb
michelleconville <michelle.conville@gmail.com>
2023-07-03 20:04:32 +0000

    Initial setup for Allauth

--- drizzle_and_shade/settings.py
+++ drizzle_and_shade/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = [
     '8000-michellecon-drizzleands-wrkmznaae5j.ws-eu101.gitpod.io',
     ]
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -42,6 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ]
 
 MIDDLEWARE = [
@@ -71,6 +74,26 @@ TEMPLATES = [
         },
     },
 ]
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+SITE_ID = 1
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
+ACCOUNT_USERNAME_MIN_LENGTH = 4
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'drizzle_and_shade.wsgi.application'
 
