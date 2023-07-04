1413129e6790d63ce03fd1b4282841c405d17af2
michelleconville <michelle.conville@gmail.com>
2023-07-04 16:32:47 +0000

    Added home app and templates

--- drizzle_and_shade/settings.py
+++ drizzle_and_shade/settings.py
@@ -45,6 +45,9 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+
+    # Apps
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -62,7 +65,10 @@ ROOT_URLCONF = 'drizzle_and_shade.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
