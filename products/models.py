73bd7db9600983dd5a5f515d32f05ca392a3cd66
michelleconville <michelle.conville@gmail.com>
2023-07-06 08:04:50 +0000

    Created model classes for catagories and products

--- products/models.py
+++ products/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    class Meta:
+        verbose_name_plural = 'Categories'
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(
+        max_digits=6, decimal_places=2, null=True, blank=True)
+    review_count = models.DecimalField(
+        max_digits=6, decimal_places=0, null=True, blank=True, default=0
+    )
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image_alt = models.CharField(max_length=100, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
