98410fd8ee3de759df1e956be57d88bf1d629b34
michelleconville <michelle.conville@gmail.com>
2023-07-06 18:02:41 +0000

    Update products model and customise admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Product, Category
 
 # Register your models here.
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+        'image_alt',
+    )
+
+    ordering = ('name',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
--- products/models.py
+++ products/models.py
@@ -27,6 +27,7 @@ class Product(models.Model):
         max_digits=6, decimal_places=0, null=True, blank=True, default=0
     )
     image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
     image_alt = models.CharField(max_length=100, null=False, blank=False)
 
     def __str__(self):
