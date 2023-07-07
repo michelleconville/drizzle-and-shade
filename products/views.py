de39a5f04115ebc6a73f90a8542fa0492e573022
michelleconville <michelle.conville@gmail.com>
2023-07-07 15:15:30 +0000

    Setup the products detail page

--- products/views.py
+++ products/views.py
@@ -1,7 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
-
-# Create your views here.
 
 
 def all_products(request):
@@ -14,3 +12,15 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to show individual umbrella details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
