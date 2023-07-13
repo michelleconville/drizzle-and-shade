55892ddc0f814d6d5aaaef66150166ce205e88d1
michelleconville <michelle.conville@gmail.com>
2023-07-13 08:49:46 +0000

    Added sorting and product counts to the product page

--- products/views.py
+++ products/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from .models import Product, Category
-from django.db.models.functions import Lower
 
 
 def all_products(request):
@@ -37,13 +36,14 @@ def all_products(request):
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request,
-                               ("You didn't enter any search criteria!"))
+                messages.error(
+                    request, "You didn't enter any search criteria!"
+                    )
                 return redirect(reverse('products'))
 
-            queries = Q(
-                name__icontains=query
-                ) | Q(description__icontains=query)
+            queries = Q(name__icontains=query) | Q(
+                description__icontains=query
+                )
             products = products.filter(queries)
 
     current_sorting = f'{sort}_{direction}'
