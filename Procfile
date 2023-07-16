09d8c8c5fe6261461a3b85f7b4807cfc775bcb3f
michelleconville <michelle.conville@gmail.com>
2023-07-16 14:52:12 +0000

    Updated settings file before deployment to Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn drizzle_and_shade.wsgi
--- drizzle_and_shade/settings.py
+++ drizzle_and_shade/settings.py
@@ -31,6 +31,8 @@ DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-michellecon-drizzleands-fqh6q52qkj0.ws-eu101.gitpod.io',
+    'drizzleandshade.herokuapp.com',
+    'localhost'
     ]
 
 # Application definition
@@ -138,12 +140,17 @@ WSGI_APPLICATION = 'drizzle_and_shade.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-     }
- }
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
