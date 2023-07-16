a9e6d34bbfe4f55c49fc42042f3d9dc6140e06f6
michelleconville <michelle.conville@gmail.com>
2023-07-16 14:40:46 +0000

    Update setting in advance of deployment

--- drizzle_and_shade/settings.py
+++ drizzle_and_shade/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 
 if os.path.exists('eny.py'):
@@ -138,11 +139,11 @@ WSGI_APPLICATION = 'drizzle_and_shade.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     }
+ }
 
 
 # Password validation
