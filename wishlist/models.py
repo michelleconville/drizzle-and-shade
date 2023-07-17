dfa6ef3309307938f2257df3c7153072eec9c9cc
michelleconville <michelle.conville@gmail.com>
2023-07-17 19:27:07 +0000

    Created wishlist app

---
+++ wishlist/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from products.models import Product
+from profiles.models import UserProfile
+
+
+class Wishlist(models.Model):
+    """ Wishlist model to store users favourite books"""
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE,
+                             related_name='user_wishlist',
+                             null=False, blank=False)
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, null=False, blank=False)
+
+    def __str__(self):
+        return self.product.name
