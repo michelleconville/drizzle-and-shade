a9caa59fe4e2a4483fb030d8167cd1e3d1ea90b5
michelleconville <michelle.conville@gmail.com>
2023-07-22 19:25:32 +0000

    Fix: Remove newsletter from main urls file

--- drizzle_and_shade/urls.py
+++ drizzle_and_shade/urls.py
@@ -29,6 +29,5 @@ urlpatterns = [
     # path('profile/', include('profiles.urls')),
     path('profiles/', include('profiles.urls')),
     path('wishlist/', include('wishlist.urls')),
-    path('newsletter/', include('newsletter.urls')),
     path('marketing/', include('marketing.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
