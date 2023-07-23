c3c6a9869c162f85356d64fea5dc4fca2ef77f8b
michelleconville <michelle.conville@gmail.com>
2023-07-23 19:02:46 +0000

    Add an add_url template, url and view

--- faqs/urls.py
+++ faqs/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.faqs, name='faqs'),
+    path('add_faqs/', views.add_faqs, name='add_faqs'),
+    path('edit_faqs/<int:item_id>/', views.edit_faqs, name='edit_faqs'),
+    path('delete_faqs/<int:item_id>/', views.delete_faqs, name='delete_faqs'),
 ]
--- faqs/views.py
+++ faqs/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import Faqs
+from .forms import FaqsForm
 
 
 def faqs(request):
@@ -17,3 +18,80 @@ def faqs(request):
     }
 
     return render(request, template, context)
+
+
+@login_required
+def add_faqs(request):
+    """
+    Add a new faqs to the store
+    """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry! You are not authorised to do that')
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        form = FaqsForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'FAQs added successfully.')
+            return redirect('faqs')
+        else:
+            messages.info(
+                request, 'Failed to update FAQ.\
+                     Please ensure the form is valid.'
+            )
+    else:
+        form = FaqsForm()
+
+    template = 'faqs/add_faqs.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def edit_faqs(request, item_id):
+    """
+    Edit a faqs and update to the store
+    """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry! You are not authorised to do that')
+        return redirect(reverse('home'))
+
+    item = get_object_or_404(Faqs, pk=item_id)
+
+    if request.method == 'POST':
+        form = FaqsForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'FAQs updated successfully.')
+            return redirect('faqs')
+        else:
+            messages.info(request, 'Failed to update product.\
+                Please ensure the form is valid.')
+    else:
+        form = FaqsForm(instance=item)
+
+    template = 'faqs/edit_faqs.html'
+    context = {
+        'form': form,
+        'item': item,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def delete_faqs(request, item_id):
+    """
+    Delete a faqs from the store
+    """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry! You are not authorised to do that')
+        return redirect(reverse('home'))
+    item = get_object_or_404(Faqs, pk=item_id)
+    item.delete()
+    messages.success(request, f'{item} is deleted successfully')
+    return redirect('faqs')
